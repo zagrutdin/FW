7a2414c22b1205b6cb33b1f1cd315c7a0974a88d
Флорит Загрутдинов <zagrutdin@gmail.com>
2025-09-23 12:36:05 +0300

    Update 8822FW-test.ps1

--- 8822FW-test.ps1
+++ 8822FW-test.ps1
@@ -1,81 +1,12 @@
-# Автор: Флорит Загрутдинов
-# Телефон: +7 917 272-22-88
-
-$PortList = @(22,80,443)
-
-
-$IPList = @"
-10.148.196.131
-10.148.196.132
-10.148.196.133
-10.148.196.134
-10.148.196.135
-10.148.196.201
-10.148.196.202
-10.148.196.140
-10.148.196.141
-10.148.196.142
-10.148.196.217
-10.148.196.145
-10.148.196.144
-10.148.196.146
-10.148.195.131
-10.148.195.132
-10.148.195.133
-10.148.195.201
-10.148.195.202
-10.148.195.141
-10.148.195.142
-"@ -split "`n"
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# ==================== Функции ====================
-
-# Метод ping
 function Test-Ping {
     param([string]$IP)
     try {
-        $reply = Test-Connection -ComputerName $IP -Count 1 -ErrorAction Stop | Select-Object -First 1
-        if ($reply) { return $reply.ResponseTime } else { return "-" }
+        $ping = New-Object System.Net.NetworkInformation.Ping
+        $reply = $ping.Send($IP, 1000)
+        if ($reply.Status -eq "Success") { return $reply.RoundtripTime } else { return "-" }
     } catch { return "-" }
 }
 
-# Метод проверки TCP-порта
 function Test-Port {
     param(
         [string]$IP,
@@ -85,15 +16,16 @@ function Test-Port {
     $openPorts = @()
     foreach ($port in $Ports) {
         try {
-            $result = Test-NetConnection -ComputerName $IP -Port $port -InformationLevel Quiet
-            if ($result) { $openPorts += $port }
+            $tcp = New-Object System.Net.Sockets.TcpClient
+            $tcp.Connect($IP, $port)
+            $tcp.Close()
+            $openPorts += $port
         } catch {}
     }
     if ($openPorts.Count -eq 0) { return "-" }
     else { return ($openPorts -join ",") }
 }
 
-# Проверка всех IP и сбор данных
 function Check-AllIPs {
     param(
         [string[]]$IPList,
@@ -109,7 +41,6 @@ function Check-AllIPs {
     }
 }
 
-# Вывод таблицы
 function Update-Table {
     param([hashtable]$Results)
     Clear-Host
@@ -126,8 +57,7 @@ function Update-Table {
         }
 }
 
-
-
+# ==================== Основное выполнение ====================
 $Results = @{}
 foreach ($ip in $IPList) { $Results[$ip] = $null }
 
@@ -136,32 +66,3 @@ while ($true) {
     Update-Table -Results $Results
     Start-Sleep -Seconds 5
 }
-
-
-
-WARNING: Name resolution of 10.148.196.145 failed
-WARNING: Name resolution of 10.148.196.144 failed
-WARNING: Name resolution of 10.148.196.144 failed
-WARNING: Name resolution of 10.148.196.144 failed
-WARNING: Name resolution of 10.148.196.146 failed
-WARNING: Name resolution of 10.148.196.146 failed
-WARNING: Name resolution of 10.148.196.146 failed
-WARNING: Name resolution of 10.148.195.131 failed
-WARNING: Name resolution of 10.148.195.131 failed
-WARNING: Name resolution of 10.148.195.131 failed
-WARNING: Name resolution of 10.148.195.132 failed
-WARNING: Name resolution of 10.148.195.132 failed
-WARNING: Name resolution of 10.148.195.132 failed
-WARNING: Name resolution of 10.148.195.133 failed
-WARNING: Name resolution of 10.148.195.133 failed
-WARNING: Name resolution of 10.148.195.133 failed
-WARNING: Name resolution of 10.148.195.201 failed
-WARNING: Name resolution of 10.148.195.201 failed
-WARNING: Name resolution of 10.148.195.201 failed
-WARNING: Name resolution of 10.148.195.202 failed
-WARNING: Name resolution of 10.148.195.202 failed
-WARNING: Name resolution of 10.148.195.202 failed
-WARNING: Name resolution of 10.148.195.141 failed
-WARNING: Name resolution of 10.148.195.141 failed
-WARNING: Name resolution of 10.148.195.141 failed
-WARNING: TCP connect to (10.148.195.142 : 22) failed
